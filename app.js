e92149bd4f67b5fe3364fd36c9eeb68c650bea6e
alabetskiy <labetskiy@gmail.com>
2018-04-07 15:51:06 -0400

    Added attack method

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 new Vue({
-    el:'#app',
+    el: '#app',
     data: {
-        playerHealth:100,
-        monsterHealth:100,
+        playerHealth: 100,
+        monsterHealth: 100,
         gameIsRunning: false
     },
     methods: {
-        startGame: function(){
+        startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function () {
+            var damage = Math.round(10 * Math.random());
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert('You won!!!');
+                this.gameIsRunning = false;
+            }
+            damage = Math.round(10 * Math.random());
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert('Monster won!!!');
+                this.gameIsRunning = false;
+            }
+
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
         }
     }
 })
