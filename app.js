28a3036943e32725efc7e0737e5e5ebbb7391fbc
alabetskiy <labetskiy@gmail.com>
2018-04-07 17:14:55 -0400

    Implemented attack, specialAttack function. Refactored.

--- app.js
+++ app.js
@@ -12,30 +12,66 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function () {
-            var damage = Math.round(10 * Math.random());
+            var damage = this.calculateDamage('attack');
             this.monsterHealth -= damage;
 
-            if (this.monsterHealth <= 0) {
-                alert('You won!!!');
-                this.gameIsRunning = false;
-            }
-            damage = Math.round(10 * Math.random());
-            this.playerHealth -= damage;
-
-            if (this.playerHealth <= 0) {
-                alert('Monster won!!!');
-                this.gameIsRunning = false;
+            if (this.checkWin()) {
+                return;
             }
 
+            this.monsterAttack();
+            this.checkWin();
         },
         specialAttack: function () {
+            var damage = this.calculateDamage('specialAttack');
+            this.monsterHealth -= damage;
 
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttack();
+            this.checkWin();
         },
         heal: function () {
 
         },
         giveUp: function () {
 
+        },
+        calculateDamage: function (attackType) {
+            if (attackType === 'attack') {
+                return Math.round(10 * Math.random());
+            }
+
+            if (attackType === 'specialAttack') {
+                return Math.round(30 * Math.random());
+            }
+
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    alert('You won!')
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+
+            if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        },
+        monsterAttack : function () {
+            damage = this.calculateDamage('attack');
+            this.playerHealth -= damage;
         }
     }
 })
